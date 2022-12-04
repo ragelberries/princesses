c26aa976a1e39f43537d647fba8d2ebe1d45b761
William Arvidsson <william.arvidsson@gmail.com>
2022-12-04 15:24:32 +0100

    Renaming

--- src/DressUp/DressUp.tsx
+++ src/DressUp/DressUp.tsx
@@ -21,16 +21,17 @@ const DressUp = () => {
     )
 }
 
-export interface Position {
-    x: number;
-    y: number;
-}
 
 export interface DressUpItem {
     id: number;
     url: string;
     position: Position;
     z: number;
+}
+
+export interface Position {
+    x: number;
+    y: number;
 }
 
 export default DressUp;
